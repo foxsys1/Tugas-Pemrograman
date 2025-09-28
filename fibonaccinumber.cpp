d5b3119a82b2c6e53f4c128ecca245da902ea458
foxsys1 <simonandreas769@gmail.com>
2025-09-28 22:39:38 +0700

    Create fibonaccinumber.cpp

---
+++ fibonaccinumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// ini buatan muhammad arifudin yazid ata
+int main() {
+    int digits;
+    int sums = 0;
+    cout << "How many digits do you want: ";
+    cin >> digits;
+    if (digits > 0)
+    {
+        vector<int> angga = {1,0};
+        cout << "0 ";
+        for (int i = 2; i <= digits; i++)
+    {   
+        angga.push_back(angga[i - 2] + angga[i - 1]);
+        cout << angga[i] << " ";
+        sums += angga[i];
+    }
+    cout << "\n" << "The sums of " << digits << " are " << sums;
+    }
+    else {
+        cout << "Only positive integers";
+    }
+}
